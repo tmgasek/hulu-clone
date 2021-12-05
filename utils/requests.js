90182c46ac00b5a9422faf936e3fbe9241f28215
tmgasek <tomaszgasek28@gmail.com>
2021-12-05 15:56:47 +0000

    fixed language

--- utils/requests.js
+++ utils/requests.js
@@ -11,38 +11,38 @@ export default {
   },
   fetchAction: {
     title: 'Action',
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=28&language=en-US`,
   },
   fetchComedy: {
     title: 'Comedy',
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=35&language=en-US`,
   },
   fetchHorror: {
     title: 'Horror',
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=27&language=en-US`,
   },
   fetchRomance: {
     title: 'Romance',
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10749&language=en-US`,
   },
   fetchMystery: {
     title: 'Mystery',
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=9648&language=en-US`,
   },
   fetchSciFi: {
     title: 'SciFi',
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=878&language=en-US`,
   },
   fetchWestern: {
     title: 'Western',
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=37&language=en-US`,
   },
   fetchAnimation: {
     title: 'Animation',
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=18`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=18&language=en-US`,
   },
   fetchTV: {
     title: 'TV',
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770&language=en-US`,
   },
 };
