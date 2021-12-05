c2cfe427657c28cd556b0101a75244100c4e3726
tmgasek <tomaszgasek28@gmail.com>
2021-12-05 13:05:24 +0000

    navbar done

---
+++ components/Header.js
@@ -0,0 +1,35 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
+import Image from 'next/image';
+import HeaderItem from './HeaderItem';
+
+function Header() {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+
+      <Image
+        className="object-contain"
+        src="/Hulu_Logo.svg"
+        alt=""
+        height={75}
+        width={150}
+      />
+    </header>
+  );
+}
+
+export default Header;
