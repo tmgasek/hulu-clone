8f584d6ca74594283f1ad83f333c6e51bd170b38
tmgasek <tomaszgasek28@gmail.com>
2021-12-12 15:52:13 +0000

    first section mobile view done

--- pages/home.js
+++ pages/home.js
@@ -4,52 +4,55 @@ function HomePage() {
   //this works style={{ backgroundImage: 'url("/header.jpg")' }}
 
   return (
-    <div>
+    <div className="text-white">
       <Head>
         <title>Stream TV and Movies Live and Online | Hulu</title>
       </Head>
-      <header
-        style={{ backgroundImage: 'url("/header.jpg")' }}
-        className="bg-cover bg-right bg-no-repeat h-[740px] after:absolute after:top-0 after:w-full after:h-32 after:bg-header-shadow after:z-1 "
-      >
+      <header className="bg-headerSm md:bg-headerMd bg-cover bg-right bg-no-repeat after:absolute after:top-0 after:w-full after:h-32 after:bg-header-shadow after:z-1 ">
         <nav className="flex justify-end p-5 z-10 relative">
-          <button className="cursor-pointer uppercase font-bold text-white">
+          <button className="cursor-pointer uppercase font-bold ">
             Log In
           </button>
         </nav>
-        <div className=" flex flex-col justify-between h-[678px]">
+
+        <div className=" flex flex-col justify-between h-auto">
           <div className="flex flex-col text-xl items-center mt-4 ">
-            <h4 className="uppercase ">bundle with any hulu plan & save</h4>
+            <h4 className="uppercase py-2 font-bold tracking-widest text-sm text-hulu ">
+              bundle with any hulu plan & save
+            </h4>
             <div className="w-1/2">
               <img src="/logos.png" />
             </div>
-            <div className="font-bold text-2xl p-4  text-center">
+            <div className="font-extrabold  text-2xl p-4  text-center">
               Get endless entertainment, live sports, and the shows and movies
               you love.
             </div>
-            <button className="uppercase bg-white text-gray-900 p-4 px-6 rounded-lg">
+            <button className="uppercase bg-white text-black p-4 px-8 rounded-lg font-semibold text-sm tracking-widest mt-4">
               get the disney bundle
             </button>
             <p className="text-sm p-6">See details and bundle terms</p>
           </div>
-          <div className=" h-40 bg-black/50 flex justify-evenly items-center">
-            <div className="w-40">
-              <img src="/logo.png" className="h-12" />
+          <div className="p-4  bg-black/50 flex flex-col gap-6 md:gap-0 md:flex-row justify-evenly items-center ">
+            <div className="w-60 ">
+              <img src="/logo.png" className="h-12 m-auto" />
             </div>
-            <div className="text-center w-96 ">
-              <p className="uppercase py-1">try up to one month free</p>
-              <p className="text-3xl">
+            <div className="text-center max-w-xl ">
+              <p className="uppercase py-2 font-bold tracking-widest text-sm text-hulu ">
+                try up to one month free
+              </p>
+              <p className="text-sm md:text-2xl">
                 Here just for Hulu? Get thousands of TV shows and movies.
               </p>
             </div>
-            <div className="w-40">
-              <button className="uppercase border-2 rounded-sm px-4">
+            <div className="w-auto">
+              <button className="uppercase border-2 rounded-lg p-4 px-6">
                 start your free trial
               </button>
             </div>
           </div>
         </div>
       </header>
+      <section>hello</section>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,19 @@ module.exports = {
     extend: {
       fontFamily: {
         hulu: ['Khula'],
+        graphik: ['Graphik Web Regular'],
       },
       screens: {
         '3xl': '2000px',
       },
       backgroundImage: {
-        header: "url('/header.jpg')",
+        headerMd: "url('/header.jpg')",
+        headerSm: "url('/header-sm.jpg')",
         'header-shadow':
           'linear-gradient(180deg,rgba(0, 0, 0, 0.76) 15.54%, rgba(0, 0, 0, 0.192) 60.23%, rgba(0, 0, 0, 8e-5) 100%)',
+      },
+      colors: {
+        hulu: '#1ce783',
       },
     },
   },
